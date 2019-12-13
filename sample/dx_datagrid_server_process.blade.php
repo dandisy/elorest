708d12d6b701727e9bda746d64937d5d5d4309a1
dandisy <dandi.webdev@gmail.com>
2019-12-13 10:58:22 +0700

    adding sample for dxDatagrid server processing

---
+++ sample/dx_datagrid_server_process.blade.php
@@ -0,0 +1,152 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/dxDataGrid/css/dx.common.css') }}" />
+    <link rel="dx-theme" data-theme="generic.light" href="{{ asset('vendor/dxDataGrid/css/dx.light.css') }}" />
+    <style>
+        .dx-datagrid .dx-data-row > td.bullet {
+            padding-top: 0;
+            padding-bottom: 0;
+        }
+        .dx-datagrid-content .dx-datagrid-table .dx-row .dx-command-edit {
+            width: auto;
+            min-width: 140px;
+        }
+    </style>
+@endsection
+
+@section('contents')
+<h3>test</h3>
+<div class="dx-viewport">
+    <div class="demo-container">
+        <div id="gridContainer"></div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('vendor/dxDataGrid/js/cldr.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/cldr/event.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/cldr/supplemental.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/cldr/unresolved.min.js') }}"></script>	
+    <script src="{{ asset('vendor/dxDataGrid/js/globalize.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/globalize/message.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/globalize/number.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/globalize/date.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/globalize/currency.min.js') }}"></script>
+    <script src="{{ asset('vendor/dxDataGrid/js/dx.web.js') }}"></script>
+    <script>
+        var gridDataSource = new DevExpress.data.DataSource({
+            key: "id",
+            load: function(loadOptions) {
+                var d = $.Deferred(),
+                        params = {};
+                [
+                    "skip",     
+                    "take", 
+                    "requireTotalCount", 
+                    "requireGroupCount", 
+                    "sort", 
+                    "filter", 
+                    "totalSummary", 
+                    "group", 
+                    "groupSummary"
+                ].forEach(function(i) {
+                    if(i in loadOptions && isNotEmpty(loadOptions[i])) 
+                        params[i] = JSON.stringify(loadOptions[i]);
+                });
+                $.getJSON("{{url('dxDatagrid/User')}}", params)
+                    .done(function(result) {
+                        console.log(result);
+                        d.resolve(result.data, { 
+                            totalCount: result.totalCount,
+                            summary: result.summary,
+                            groupCount: result.groupCount
+                        });
+                    });
+                return d.promise();
+            }
+        });
+
+        function isNotEmpty(value) {
+            return value !== undefined && value !== null && value !== "";
+        }
+
+        $("#gridContainer").dxDataGrid({
+            dataSource: gridDataSource,
+            remoteOperations: { groupPaging: true },
+            columnAutoWidth: true,
+            allowColumnResizing: true,
+            columnResizingMode: 'widget', // or 'nextColumn'
+            rowAlternationEnabled: true,
+            hoverStateEnabled: true, 
+            // // showBorders: true,
+            grouping: {
+                autoExpandAll: false,
+                contextMenuEnabled: true
+            },
+            groupPanel: {
+                visible: true
+            },       
+            searchPanel: {
+                visible: true
+            },   
+            filterRow: {
+                visible: true
+            },
+            headerFilter: {
+                visible: true
+            },
+            columnChooser: {
+                enabled: true,
+                mode: "dragAndDrop" // or "select"
+            },
+            columnFixing: {
+                enabled: true
+            },
+            // selection: {
+            //     mode: "multiple",
+            //     // allowSelectAll : false,
+            //     selectAllMode: 'page',
+            //     showCheckBoxesMode : "always"
+            // },
+            // height: 420,            
+            paging: {
+                pageSize: 10
+            },
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [10, 50, 100],
+                showInfo: true
+            },
+            keyExpr: "id",
+            columns: [
+                {
+                    caption: "#",
+                    allowEditing: false,
+                    allowSorting: false,
+                    cellTemplate: function (cellElement, cellInfo) {
+                        cellElement.html('<span>'+(parseInt(cellInfo.rowIndex, 10)+1)+'</span>');
+                    }
+                },
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at'
+            ],
+            summary: {
+                totalItems: [
+                    {
+                        column: "id",
+                        summaryType: "sum"
+                    }, 
+                    {
+                        column: "name",
+                        summaryType: "count"
+                    }
+                ]
+            }
+        });
+    </script>
+@endsection
---
+++ sample/routes/api.php
@@ -0,0 +1,223 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('dxDatagrid/{model}', function($model, Request $request) {
+    $res['info'] = [];
+    $res['info']['request'] = $request->all();
+    
+    $modelNS = '\App\Models\\'.$model;
+    $data = new $modelNS();
+    
+    if($request->filter) {
+        $filter = json_decode($request->filter);
+        
+        // $res['info']['filter'] = $filter;
+        
+        if($filter[1] == 'and' || $filter[1] == 'or') {
+            if($filter[1] == 'and') {
+                if($filter[2][1] == 'or') {
+                    if($filter[0][1] == 'and') {
+                        foreach($filter[0] as $k0 => $f0) {
+                            if($k0 % 2 == 0) {
+                                if($f0[1] == 'contains') {
+                                    $f0[1] = 'like';
+                                    $f0[2] = '%'.$f0[2].'%';
+                                }
+                                
+                                $data = $data->where($f0[0], $f0[1], $f0[2]);
+                            }
+                        }
+                    } else {
+                        if($filter[0][1] == 'contains') {
+                            $filter[0][1] = 'like';
+                            $filter[0][2] = '%'.$filter[0][2].'%';
+                        }
+                        
+                        $data = $data->where($filter[0][0], $filter[0][1], $filter[0][2]);
+                    }
+                    
+                    $data = $data->where(function($query) use ($filter) {
+                        foreach($filter[2] as $k2 => $f2) {
+                            if($k2 % 2 == 0) {
+                                if($f2[1] == 'contains') {
+                                    $f2[1] = 'like';
+                                    $f2[2] = '%'.$f2[2].'%';
+                                }
+                                
+                                $query = $query->orWhere($f2[0], $f2[1], $f2[2]);
+                            }
+                        }
+                    });
+                } else {
+                    foreach($filter as $k1 => $f1) {
+                        if($k1 % 2 == 0) {
+                            if($f1[1] == 'contains') {
+                                $f1[1] = 'like';
+                                $f1[2] = '%'.$f1[2].'%';
+                            }
+                            
+                            $data = $data->where($f1[0], $f1[1], $f1[2]);
+                        }
+                    }
+                }
+            }
+            if($filter[1] == 'or') {
+                foreach($filter as $k1 => $f1) {
+                    if($k1 % 2 == 0) {
+                        if($f1[1] == 'contains') {
+                            $f1[1] = 'like';
+                            $f1[2] = '%'.$f1[2].'%';
+                        }
+                        
+                        $data = $data->orWhere($f1[0], $f1[1], $f1[2]);
+                    }
+                }
+            }
+        } else {
+            if($filter[1] == 'contains') {
+                $filter[1] = 'like';
+                $filter[2] = '%'.$filter[2].'%';
+            }
+            
+            $data = $data->where($filter[0], $filter[1], $filter[2]);
+        }
+    }
+    
+    // TODO : cek logic untuk yang custom summary
+    if($request->totalSummary) {
+        $res['summary'] = [];
+        $summaries = json_decode($request->totalSummary);
+        
+        foreach($summaries as $summary) {
+            $cmd = $summary->summaryType;
+            array_push($res['summary'], $data->$cmd($summary->selector));
+        }
+    }
+    
+    if($request->requireTotalCount == 'true') {
+        $res['totalCount'] = $data->count();
+    }
+    
+    if($request->group) {
+        $resData = [];        
+        $groups = json_decode($request->group); 
+        
+        // TODO : cek lagi apakah group selalu array count = 1 atau bisa lebih dari 1 sehingga butuh foreach
+        $gData0 = $data->groupBy($groups[0]->selector);
+        
+        if($request->requireGroupCount == 'true') {
+            $res['groupCount'] = count($gData0->get()->toArray());
+        }
+        
+        if($groups[0]->desc) {
+            $gData0 = $gData0->orderBy($groups[0]->selector, 'desc');
+        } else {
+            $gData0 = $gData0->orderBy($groups[0]->selector);
+        }
+        
+        $res['info']['sql'] = $gData0->toSql();
+        
+        // group inner data
+        $gData0 = $gData0->get();
+        foreach($gData0 as $key => $item) {
+            $resData[$key] = [];
+            $resData[$key]['key'] = $item->{$groups[0]->selector};
+            
+            $inGroupData = $data->where($groups[0]->selector, $item->{$groups[0]->selector});
+            
+            if($request->sort) {
+                $sorts = json_decode($request->sort);
+                
+                // data yang di group by kemudian di sort, array sort-nya bisa lebih dari 1 sehingga butuh foreach
+                foreach($sorts as $sort) {
+                    if(isset($sort->isExpanded)) {
+                        // TODO : terjadi ketika kolom sevagai group by di sort
+                        if($sort->isExpanded) {
+                            //
+                        }
+                    }
+                    
+                    if($sort->desc) {
+                        $inGroupData = $inGroupData->orderBy($sort->selector, 'desc');
+                    } else {
+                        $inGroupData = $inGroupData->orderBy($sort->selector);
+                    }
+                }
+            }
+            
+            if($groups[0]->isExpanded == 'true') {
+                $resData[$key]['items'] = $inGroupData->get();
+            } else {
+                $resData[$key]['items'] = null;
+            }
+            
+            $resData[$key]['count'] =  $inGroupData->count();
+            
+            // TODO : cek hasilnya apakah sudah OK, dan cek logic untuk yang custom summary
+            if($request->groupSummary) {
+                $resData[$key]['summary'] = [];
+                $gSummaries = json_decode($request->groupSummary);
+                
+                foreach($gSummaries as $gSummary) {
+                    $gCmd = $gSummary->summaryType;
+                    array_push($resData[$key]['summary'], $inGroupData->$gCmd($gSummary->selector));
+                }
+            }
+        }
+        
+        // group header data
+        $resData = collect($resData);
+        if($request->skip) {
+            $resData = $resData->slice($request->skip);
+        }        
+        $res['data'] = $resData->take($request->take);
+    } else {
+        if($request->sort) {
+            $sorts = json_decode($request->sort);
+            
+            // TODO : cek apakah sort pada data yang tidak di grouping selalu array count = 1 atau bisa lebih dari 1 sehingga butuh foreach
+            foreach($sorts as $sort) {
+                if(isset($sort->isExpanded)) {
+                    // TODO : terjadi ketika kolom sevagai group by di sort
+                    if($sort->isExpanded) {
+                        //
+                    }
+                }
+                
+                if($sort->desc) {
+                    $data = $data->orderBy($sort->selector, 'desc');
+                } else {
+                    $data = $data->orderBy($sort->selector);
+                }
+            }
+        }
+        
+        if($request->skip) {
+            $data = $data->skip($request->skip);
+        }
+        if($request->take) {
+            $data = $data->take($request->take);
+        }
+        
+        $res['info']['sql'] = $data->toSql();
+        
+        $res['data'] = $data->get();
+    }
+    
+    return $res;
+    
+    // return response(json_encode($res, 200))
+    //     ->header('Content-Type', 'application/json');
+});
